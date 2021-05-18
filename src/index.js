31c70aae8742781dca5c27853807486a8399c551
solentz <4okayz07@solent.ac.uk>
2021-05-18 22:54:30 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsTemplate = require("ejs");
+const PORT = process.env.port || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "html");
+app.engine("html", ejsTemplate.renderFile);
+app.set("views", __dirname + "/views");
+
+/**
+ *
+ */
+app.get("/", (request, response) => {
+  response.render("index", {
+    pageTitle: "ZooLand || Home Page",
+    pageDescription: "ZooLand is  a website for children",
+  });
+});
+
+//
+app.listen(PORT, (request, response) => {
+  console.log(`application is running on port http://localhost:${PORT}`);
+});
---
+++ src/public/js/navbar.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-bars");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("slide");
+});
